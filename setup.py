a685234fa265fc165e1e3b90c39fe673a3de7e6d
Oliver Schmidhauser <oli@glow.li>
2017-08-05 19:18:28 +0200

    Update code for mpd

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 from __future__ import unicode_literals, print_function
 import sys
-from termuxmpv import __version__
+from termuxmpdnotifications import __version__
 from setuptools import setup
 import os
 
@@ -13,22 +13,19 @@ def read_reqs(path):
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
-# requires = read_reqs('requirements.txt')
+requires = read_reqs('requirements.txt')
 setup(
-    name='Termux-Mpv',
+    name='Termux-MPD-Notifications',
     version=__version__,
-    description='Wrapper for Mpv to display notification in Android/Termux',
+    description='Daemon to display Android Notifications for MPD on Termux',
     author='Oliver Schmidhauser',
     author_email='oli@glow.li',
         url='oliverse.ch',
-    # long_description=(
-        # "Virtual pet simulator"
-    # ),
-    packages=[str('termuxmpv')],
+    packages=[str('termuxmpdnotifications')],
         # data_files=[('translations', ['translations/en.yml','translations/de.yml']),
                              # ('config',['config/default.yml'])],
     license='',
     platforms='Linux x86, x86-64',
-    # install_requires=requires,
-    entry_points={'console_scripts': ['termuxmpv = termuxmpv.__main__:main']},
+    install_requires=requires,
+    entry_points={'console_scripts': ['termux-mpd-notifications = termuxmpdnotifications.__main__:main']},
 )
---
+++ termuxmpdnotifications/__main__.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+import argparse
+import sys
+import os
+import subprocess
+from mpd import MPDClient
+import mpd
+import time
+import argparse
+import signal
+class termuxmpdnotifications:
+    def cleanup(self,signum,frame):
+        self.removeNotification()
+        try:
+            self.client.close()
+        except:
+            pass
+        self.updatehook()
+    def __init__(self,args):
+        # atexit.register(self.cleanup)
+        signal.signal(signal.SIGTERM, self.cleanup)
+        self.visible=False
+        parser = argparse.ArgumentParser()
+        parser.description="Best viewed when piped into `less -RS`"
+        parser.add_argument('--host', help='Host')
+        parser.add_argument('--port', default="6600", help='Port')
+        options = parser.parse_args()
+
+
+        self.client=MPDClient()
+        self.notificationId=str(time.time())
+        if options.host:
+            self.host=options.host
+        elif 'MPD_HOST' in os.environ:
+            self.host=os.environ['MPD_HOST']
+        else:
+            self.host="localhost"
+        self.port=options.port
+        self.mpcinfo="-h {} -p {}".format(self.host,self.port)
+        try:
+            self.client.connect(self.host,self.port)
+            self.err("Connected to MPD Version {}".format(self.client.mpd_version))
+            while True:
+                self.status=self.client.status()
+                self.metadata=self.client.currentsong()
+                if self.status["state"]=="stop":
+                    self.removeNotification()
+                else:
+                    self.updateNotification()
+                self.client.idle()
+        except mpd.ConnectionError:
+            self.err("Connection Aborted.")
+        except ConnectionRefusedError:
+            self.err("Connection Refused.")
+    def err(self,out):
+        print(out,file=sys.stderr)
+    def removeNotification(self):
+        if self.visible==True:
+            self.visible=False
+            command=["termux-notification-remove",self.notificationId]
+            output=subprocess.call(command)
+    def updatehook(self):
+        command="song-change-hook"
+        devnull=open(os.devnull, 'wb')
+        try:
+            subprocess.call(["sh", "-c", command],stdout=devnull,stderr=devnull)
+        except:
+            pass
+    def updateNotification(self):
+        self.updatehook()
+        self.visible=True
+        # padding="           "
+        #disable padding for now
+        padding=""
+        # playbutton="{}⏸{}".format(padding,padding)
+        # prevbutton="{}⏮{}".format(padding,padding)
+        # nextbutton="{}⏭{}".format(padding,padding)
+        playbutton="{}❙❙{}".format(padding,padding)
+        prevbutton="{}|◀◀{}".format(padding,padding)
+        nextbutton="{}▶▶|{}".format(padding,padding)
+        if self.status["state"] == "pause":
+            playbutton="{} ▶ {}".format(padding,padding)
+        metadata={}
+        for attr in ["album","artist","title"]:
+            try:
+                metadata[attr]=self.metadata[attr]
+            except KeyError:
+                try:
+                    metadata[attr]=self.metadata[attr.upper()]
+                except KeyError:
+                    metadata[attr]="None"
+        if metadata["title"]=="None":
+            title="Unknown"
+        else:
+            title=metadata["title"]
+        command=[
+            "termux-notification",
+            "--id", self.notificationId,
+            "--title", title,
+            "--content", "{}, {}".format(metadata["artist"], metadata["album"]),
+            "--priority", "max",
+            # "--action", ";".join([
+                # "am start --user 0 -n com.termux/com.termux.app.TermuxActivity",
+                # "echo 'updateNotification'> {}".format(self.fifoname)
+                # ]),
+            "--button1", prevbutton,
+            "--button1-action","mpc prev {}".format(self.mpcinfo),
+            "--button2", playbutton,
+            "--button2-action","mpc toggle {}".format(self.mpcinfo),
+            "--button3", nextbutton,
+            "--button3-action","mpc next {}".format(self.mpcinfo),
+            "--on-delete", "mpc stop {}".format(self.mpcinfo),
+        ]
+        output=subprocess.call(command)
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+    termuxmpdnotifications(args)
+if __name__ == "__main__":
+    main()
+
+
